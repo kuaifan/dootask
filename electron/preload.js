ddeaec47b69528b35773815b0308e17c23ba3fca
kuaifan <aipaw@live.cn>
2021-07-07 18:37:00 +0800

    no message

---
+++ electron/preload.js
@@ -0,0 +1,14 @@
+// preload.js
+
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector)
+        if (element) element.innerText = text
+    }
+
+    for (const dependency of ['chrome', 'node', 'electron']) {
+        replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+})
