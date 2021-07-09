b5b00685f94b1e6aecd220b8d3c0609cbc9b5e7a
aipaw <aipaw@qq.com>
2021-07-09 09:56:14 +0800

    no message

--- electron/main.js
+++ electron/main.js
@@ -1,11 +1,14 @@
-// main.js
-
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 const path = require('path')
+let willQuitApp = false;
 
-function createWindow () {
-    // Create the browser window.
+function getCounterValue(title) {
+    const itemCountRegex = /[([{]([\d.,]*)\+?[}\])]/;
+    const match = itemCountRegex.exec(title);
+    return match ? match[1] : undefined;
+}
+
+function createWindow(setDockBadge) {
     const mainWindow = new BrowserWindow({
         width: 1280,
         height: 800,
@@ -14,34 +17,39 @@ function createWindow () {
         }
     })
 
-    // and load the index.html of the app.
     mainWindow.loadFile('./public/index.html').then(r => {
 
     })
 
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    mainWindow.on('page-title-updated', function (event, title) {
+        const counterValue = getCounterValue(title);
+        if (counterValue) {
+            setDockBadge(counterValue);
+        } else {
+            setDockBadge('');
+        }
+    })
+
+    mainWindow.on('close', function (e) {
+        if (!willQuitApp) {
+            e.preventDefault();
+            app.hide();
+        }
+    })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// 部分 API 在 ready 事件触发后才能使用。
 app.whenReady().then(() => {
-    createWindow()
+    createWindow(app.dock.setBadge)
 
     app.on('activate', function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+        if (BrowserWindow.getAllWindows().length === 0) createWindow(app.dock.setBadge)
     })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. 也可以拆分成几个文件，然后用 require 导入。
+app.on('before-quit', () => {
+    willQuitApp = true
+});
