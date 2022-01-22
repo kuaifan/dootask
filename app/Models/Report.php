3ba6ea9c7e4f1e8aeecf37903235c9a0c073f494
Mr.Huan <huanzi20100702@hotmail.com>
2022-01-22 15:07:37 +0800

    perf: 调整周报签名的生成方法

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -144,15 +144,15 @@ class Report extends AbstractModel
                 // 如果设置了周期偏移量
                 empty( $offset ) || $now_dt->subWeeks( abs( $offset ) );
                 $now_dt->startOfWeek(); // 设置为当周第一天
-                return $now_dt->year . $now_dt->weekOfYear . $now_dt->month . $now_dt->weekOfMonth;
+                return $now_dt->year . $now_dt->weekOfYear;
             },
             Report::DAILY => function() use ($now_dt, $offset) {
                 // 如果设置了周期偏移量
                 empty( $offset ) || $now_dt->subDays( abs( $offset ) );
-                return $now_dt->year . $now_dt->dayOfYear . $now_dt->month . $now_dt->daysInMonth;
+                return $now_dt->format("Ymd");
             },
             default => "",
         };
-        return md5( $user->userid . ( is_callable($time_s) ? $time_s() : "" ) . $type );
+        return $user->userid . ( is_callable($time_s) ? $time_s() : "" );
     }
 }
