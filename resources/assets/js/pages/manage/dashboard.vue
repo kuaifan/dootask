e969b5b7e4522b70f9985bebc4672f67b6673a37
kuaifan <aipaw@live.cn>
2022-03-05 10:59:18 +0800

    perf: 优化仪表盘使用sticky方式

--- resources/assets/js/pages/manage/dashboard.vue
+++ resources/assets/js/pages/manage/dashboard.vue
@@ -5,72 +5,78 @@
             <div class="dashboard-hello">{{$L('欢迎您，' + userInfo.nickname)}}</div>
             <div class="dashboard-desc">{{$L('以下是你当前的任务统计数据')}}</div>
             <ul class="dashboard-block">
-                <li @click="dashboard='today'">
-                    <div class="block-title">{{$L('今日待完成')}}</div>
+                <li @click="scrollTo('today')">
+                    <div class="block-title">{{getTitle('today')}}</div>
                     <div class="block-data">
                         <div class="block-num">{{dashboardTask.today.length}}</div>
                         <i class="taskfont">&#xe6f4;</i>
                     </div>
                 </li>
-                <li @click="dashboard='overdue'">
-                    <div class="block-title">{{$L('超期未完成')}}</div>
+                <li @click="scrollTo('overdue')">
+                    <div class="block-title">{{getTitle('overdue')}}</div>
                     <div class="block-data">
                         <div class="block-num">{{dashboardTask.overdue.length}}</div>
                         <i class="taskfont">&#xe603;</i>
                     </div>
                 </li>
-                <li @click="dashboard='all'">
-                    <div class="block-title">{{$L('待完成任务')}}</div>
+                <li @click="scrollTo('all')">
+                    <div class="block-title">{{getTitle('all')}}</div>
                     <div class="block-data">
                         <div class="block-num">{{dashboardTask.all.length}}</div>
                         <i class="taskfont">&#xe6f9;</i>
                     </div>
                 </li>
             </ul>
-            <template v-if="list.length > 0">
-                <div class="dashboard-title">{{title}}</div>
-                <ul class="dashboard-list overlay-y">
-                    <li
-                        v-for="(item, index) in list"
-                        :key="index"
-                        :class="{complete: item.complete_at}"
-                        :style="item.color ? {backgroundColor: item.color} : {}"
-                        @click="openTask(item)">
-                        <em
-                            v-if="item.p_name"
-                            class="priority-color"
-                            :style="{backgroundColor:item.p_color}"></em>
-                        <TaskMenu :ref="`taskMenu_${item.id}`" :task="item">
-                            <div slot="icon" class="drop-icon" @click.stop="">
-                                <i class="taskfont" v-html="item.complete_at ? '&#xe627;' : '&#xe625;'"></i>
+            <div class="dashboard-list overlay-y">
+                <div
+                    :ref="`type_${column.type}`"
+                    v-for="(column, index) in columns"
+                    v-if="column.list.length > 0"
+                    :key="index">
+                    <div class="dashboard-title">{{column.title}}</div>
+                    <ul class="dashboard-li">
+                        <li
+                            v-for="(item, index) in column.list"
+                            :key="index"
+                            :class="{complete: item.complete_at}"
+                            :style="item.color ? {backgroundColor: item.color} : {}"
+                            @click="openTask(item)">
+                            <em
+                                v-if="item.p_name"
+                                class="priority-color"
+                                :style="{backgroundColor:item.p_color}"></em>
+                            <TaskMenu :ref="`taskMenu_${item.id}`" :task="item">
+                                <div slot="icon" class="drop-icon" @click.stop="">
+                                    <i class="taskfont" v-html="item.complete_at ? '&#xe627;' : '&#xe625;'"></i>
+                                </div>
+                            </TaskMenu>
+                            <div class="item-title">
+                                <!--工作流状态-->
+                                <span v-if="item.flow_item_name" :class="item.flow_item_status" @click.stop="openMenu(item)">{{item.flow_item_name}}</span>
+                                <!--是否子任务-->
+                                <span v-if="item.sub_top === true">{{$L('子任务')}}</span>
+                                <!--有多少个子任务-->
+                                <span v-if="item.sub_my && item.sub_my.length > 0">+{{item.sub_my.length}}</span>
+                                <!--任务描述-->
+                                {{item.name}}
                             </div>
-                        </TaskMenu>
-                        <div class="item-title">
-                            <!--工作流状态-->
-                            <span v-if="item.flow_item_name" :class="item.flow_item_status" @click.stop="openMenu(item)">{{item.flow_item_name}}</span>
-                            <!--是否子任务-->
-                            <span v-if="item.sub_top === true">{{$L('子任务')}}</span>
-                            <!--有多少个子任务-->
-                            <span v-if="item.sub_my && item.sub_my.length > 0">+{{item.sub_my.length}}</span>
-                            <!--任务描述-->
-                            {{item.name}}
-                        </div>
-                        <div v-if="item.desc" class="item-icon">
-                            <i class="taskfont">&#xe71a;</i>
-                        </div>
-                        <div v-if="item.sub_num > 0" class="item-icon">
-                            <i class="taskfont">&#xe71f;</i>
-                            <em>{{item.sub_complete}}/{{item.sub_num}}</em>
-                        </div>
-                        <ETooltip v-if="item.end_at" :content="item.end_at" placement="right">
-                            <div :class="['item-icon', item.today ? 'today' : '', item.overdue ? 'overdue' : '']">
-                                <i class="taskfont">&#xe71d;</i>
-                                <em>{{expiresFormat(item.end_at)}}</em>
+                            <div v-if="item.desc" class="item-icon">
+                                <i class="taskfont">&#xe71a;</i>
                             </div>
-                        </ETooltip>
-                    </li>
-                </ul>
-            </template>
+                            <div v-if="item.sub_num > 0" class="item-icon">
+                                <i class="taskfont">&#xe71f;</i>
+                                <em>{{item.sub_complete}}/{{item.sub_num}}</em>
+                            </div>
+                            <ETooltip v-if="item.end_at" :content="item.end_at" placement="right">
+                                <div :class="['item-icon', item.today ? 'today' : '', item.overdue ? 'overdue' : '']">
+                                    <i class="taskfont">&#xe71d;</i>
+                                    <em>{{expiresFormat(item.end_at)}}</em>
+                                </div>
+                            </ETooltip>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -110,13 +116,34 @@ export default {
     },
 
     computed: {
-        ...mapState(['userInfo', 'cacheProjects', 'taskId']),
+        ...mapState(['userInfo']),
 
         ...mapGetters(['dashboardTask', 'transforTasks']),
 
-        title() {
-            const {dashboard} = this;
-            switch (dashboard) {
+        columns() {
+            let list = [];
+            ['today', 'overdue', 'all'].some(type => {
+                let data = this.transforTasks(this.dashboardTask[type]);
+                list.push({
+                    type,
+                    title: this.getTitle(type),
+                    list: data.sort((a, b) => {
+                        return $A.Date(a.end_at || "2099-12-31 23:59:59") - $A.Date(b.end_at || "2099-12-31 23:59:59");
+                    })
+                })
+            })
+            return list;
+        },
+
+        total() {
+            const {dashboardTask} = this;
+            return dashboardTask.today.length + dashboardTask.overdue.length + dashboardTask.all.length;
+        },
+    },
+
+    methods: {
+        getTitle(type) {
+            switch (type) {
                 case 'today':
                     return this.$L('今日任务');
                 case 'overdue':
@@ -128,27 +155,20 @@ export default {
             }
         },
 
-        list() {
-            const {dashboard} = this;
-            let data = [];
-            switch (dashboard) {
-                case 'today':
-                    data = this.transforTasks(this.dashboardTask.today);
-                    break
-                case 'overdue':
-                    data = this.transforTasks(this.dashboardTask.overdue);
-                    break
-                case 'all':
-                    data = this.transforTasks(this.dashboardTask.all);
-                    break
+        scrollTo(type) {
+            try {
+                this.$refs[`type_${type}`][0].scrollIntoView({
+                    behavior: 'instant',
+                    inline: 'end',
+                });
+            } catch (e) {
+                scrollIntoView(this.$refs[`type_${type}`][0], {
+                    behavior: 'instant',
+                    inline: 'end',
+                })
             }
-            return data.sort((a, b) => {
-                return $A.Date(a.end_at || "2099-12-31 23:59:59") - $A.Date(b.end_at || "2099-12-31 23:59:59");
-            });
         },
-    },
 
-    methods: {
         openTask(task) {
             this.$store.dispatch("openTask", task)
         },
