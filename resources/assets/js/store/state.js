062f0303b434053b3f13080d815631eff41b962c
kuaifan <aipaw@live.cn>
2022-05-29 19:09:27 +0800

    fix: 聊天内容加载中刷新导致无法再继续加载的情况

--- resources/assets/js/store/state.js
+++ resources/assets/js/store/state.js
@@ -24,7 +24,7 @@ const stateData = {
     cacheUserBasic: $A.getStorageArray("cacheUserBasic"),
 
     // Dialog
-    cacheDialogs: $A.getStorageArray("cacheDialogs"),
+    cacheDialogs: $A.getStorageArray("cacheDialogs").map(item => Object.assign(item, {loading: false})),
     cacheUnreads: {},
 
     // Project
