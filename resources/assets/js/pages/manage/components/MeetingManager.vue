b42721a2a3e0226f0578a8eb1f90e74eabfad181
kuaifan <aipaw@live.cn>
2022-06-01 17:33:51 +0800

    no message

--- resources/assets/js/pages/manage/components/MeetingManager.vue
+++ resources/assets/js/pages/manage/components/MeetingManager.vue
@@ -166,7 +166,7 @@ export default {
                         delete data.name;
                         delete data.msgs;
                         //
-                        $A.loadScript('//download.agora.io/sdk/release/AgoraRTC_N.js', e => {
+                        $A.loadScript('js/AgoraRTC_N-4.11.1.js', e => {
                             if (e !== null || typeof AgoraRTC !== 'object') {
                                 $A.modalError("会议组件加载失败！");
                             } else {
