c7dafa6199c0472e7bf6ff8e71b8b4f5dc80405f
kuaifan <aipaw@live.cn>
2022-07-16 17:51:51 +0800

    build

--- resources/assets/js/pages/manage/setting/index.vue
+++ resources/assets/js/pages/manage/setting/index.vue
@@ -193,15 +193,15 @@ export default {
         },
 
         onVersion() {
-            if (!this.$Electron) {
-                return
-            }
             axios.get($A.apiUrl('system/version')).then(({status, data}) => {
                 if (status === 200) {
                     let content = `${this.$L('服务器')}: ${$A.getDomain($A.apiUrl('../'))}`
                     content += `<br/>${this.$L('服务器版本')}: v${data.version}`
                     content += `<br/>${this.$L('客户端版本')}: v${this.version}`
-                    $A.modalInfo({content})
+                    $A.modalInfo({
+                        title: '版本信息',
+                        content
+                    })
                 }
             }).catch(_ => { })
         },
