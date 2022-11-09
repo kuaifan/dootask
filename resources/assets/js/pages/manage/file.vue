6112d6950a80e96faca62858e48ba481bbd37d37
kuaifan <aipaw@live.cn>
2022-11-09 14:00:39 +0800

    fix: 搜索文件选择在上层文件夹中显示时如果已经当前文件夹时没有反应的问题

--- resources/assets/js/pages/manage/file.vue
+++ resources/assets/js/pages/manage/file.vue
@@ -1125,6 +1125,7 @@ export default {
                     break;
 
                 case 'upperFolder':
+                    this.searchKey = '';
                     this.browseFolder(item.pid)
                     break;
 
