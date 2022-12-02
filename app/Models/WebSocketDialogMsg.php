39cca6ec99cc4f9c94805afe8b5b1f1f6efddd73
kuaifan <aipaw@live.cn>
2022-12-02 17:40:38 +0800

    no message

--- app/Models/WebSocketDialogMsg.php
+++ app/Models/WebSocketDialogMsg.php
@@ -678,7 +678,7 @@ class WebSocketDialogMsg extends AbstractModel
         foreach ($matchs[0] as $key => $str) {
             $herf = $matchs[2][$key];
             $title = $matchs[3][$key] ?: $herf;
-            preg_match("/\/single\/file\/(.*?)$/i", $title, $match);
+            preg_match("/\/single\/file\/(.*?)$/i", $herf, $match);
             if ($match && strlen($match[1]) >= 32) {
                 $file = File::select(['files.id', 'files.name', 'files.ext'])->join('file_links as L', 'files.id', '=', 'L.file_id')->where('L.code', $match[1])->first();
                 if ($file && $file->name) {
