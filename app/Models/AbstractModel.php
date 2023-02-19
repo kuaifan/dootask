87d7be254a259940007b9923fe7b8a4e7187b8e6
kuaifan <aipaw@live.cn>
2023-02-19 15:18:57 +0800

    no message

--- app/Models/AbstractModel.php
+++ app/Models/AbstractModel.php
@@ -21,6 +21,7 @@ use Illuminate\Support\Facades\DB;
  * @method static \Illuminate\Database\Eloquent\Model|object|static|null cancelHidden()
  * @method static \Illuminate\Database\Eloquent\Builder|static with($relations)
  * @method static \Illuminate\Database\Query\Builder|static select($columns = [])
+ * @method static \Illuminate\Database\Query\Builder|static whereIn($column, $values, $boolean = 'and', $not = false)
  * @method static \Illuminate\Database\Query\Builder|static whereNotIn($column, $values, $boolean = 'and')
  * @mixin \Eloquent
  */
