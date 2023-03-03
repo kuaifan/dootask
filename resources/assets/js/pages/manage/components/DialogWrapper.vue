5aa31d8068fe0c6a6dd4ebe6698ed064621c0e4a
kuaifan <aipaw@live.cn>
2023-03-03 20:34:36 +0800

    perf: dialog loading

--- resources/assets/js/pages/manage/components/DialogWrapper.vue
+++ resources/assets/js/pages/manage/components/DialogWrapper.vue
@@ -134,8 +134,8 @@
             @on-error="onError"
             @on-emoji="onEmoji"
             @on-show-emoji-user="onShowEmojiUser">
-            <template slot="header">
-                <div v-if="(allMsgs.length === 0 && loadMsg) || prevId > 0" class="dialog-item loading"><Loading type="pure"/></div>
+            <template #header>
+                <div v-if="(allMsgs.length === 0 && loadMsg) || prevId > 0" class="dialog-item loading"><div class="dialog-wrapper-loading"></div></div>
                 <div v-else-if="allMsgs.length === 0" class="dialog-item nothing">{{$L('暂无消息')}}</div>
             </template>
         </VirtualList>
