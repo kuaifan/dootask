11e08fea73bbb8416d4894c9f279ec8112138110
kuaifan <aipaw@live.cn>
2023-04-03 22:48:41 +0800

    build

---
+++ public/js/build/preview.36fec9a2.js
@@ -0,0 +1,1 @@
+import"./app.73eb020e.js";import{p as a}from"./app.73eb020e.js";export{a as default};
--- public/js/build/preview.e4896186.js
+++
@@ -1,1 +0,0 @@
-import"./app.d2c43e87.js";import{p as a}from"./app.d2c43e87.js";export{a as default};
