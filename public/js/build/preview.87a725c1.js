53ea6b00ab946f5b2a1e368ba77a72f39c1bb85b
weifashi <605403358@qq.com>
2023-05-17 14:54:46 +0800

    build - 去除加密 - 测试那边用

--- public/js/build/preview.87a725c1.js
+++
@@ -1,1 +0,0 @@
-import"./app.048cc46c.js";import{p as a}from"./app.048cc46c.js";export{a as default};
---
+++ public/js/build/preview.e0f5dbb1.js
@@ -0,0 +1,1 @@
+import"./app.e005b7a4.js";import{p as a}from"./app.e005b7a4.js";export{a as default};
