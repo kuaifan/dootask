00e255a4a81d2c140fc8e37801bbe3a7da086a2d
Pang <aipaw@live.cn>
2023-08-10 07:46:08 +0800

    perf: 优化时间冲突提示框

--- resources/assets/js/pages/manage/components/TaskExistTips.vue
+++ resources/assets/js/pages/manage/components/TaskExistTips.vue
@@ -1,13 +1,20 @@
 <template>
-    <Modal v-model="show" :title="$L('计划时间冲突提示')" class="task-exist-tips">
+    <Modal
+        v-model="show"
+        :title="$L('计划时间冲突提示')"
+        :styles="{
+            width: '90%',
+            maxWidth: '550px'
+        }"
+        class="task-exist-tips">
         <List :split="false" size="small">
             <ListItem v-for="(items, userid) in tipsTask" :key="userid">
                 <div class="list-content">
                     <UserAvatar :userid="userid" :size="28" :show-icon="true" :show-name="true" tooltipDisabled />
-                    <div class="list-task clearfix" v-for="(item, key) in items" :key="key">
+                    <div class="list-task" v-for="(item, key) in items" :key="key">
                         <div class="list-task-info">
                             <span>[{{ item.project_name }}] </span>
-                            <span>{{ item.name }}</span>
+                            <span :title="item.name">{{ item.name }}</span>
                         </div>
                         <div class="list-task-date">{{ getCutTime(item) }}</div>
                     </div>
