71fb50b4ceeced80ea73263ce5ea0f4634a97a9b
weifashi <605403358@qq.com>
2023-10-20 18:43:44 +0800

    build：编译资源上传

--- public/js/build/preview.b9ada120.js
+++
@@ -1,1 +0,0 @@
-import"./app.ed197644.js";import{A as f}from"./app.ed197644.js";export{f as default};
---
+++ public/js/build/preview.f06f6516.js
@@ -0,0 +1,1 @@
+import"./app.00b87814.js";import{A as f}from"./app.00b87814.js";export{f as default};
