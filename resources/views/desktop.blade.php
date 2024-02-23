58407af2ba93568e2b455388b45e80a0f93da80e
weifs <605403358@qq.com>
2024-02-23 09:40:00 +0800

    fix: 更改其他版本的链接

--- resources/views/desktop.blade.php
+++ resources/views/desktop.blade.php
@@ -181,7 +181,7 @@
 
 <h1 class="mirror-nav">
     <p>Download of v{{ $version }}</p>
-    <a class="download-other-btn" href="https://www.dootask.com/desktop/publish/latest" target="_blank">Other Versions</a>
+    <a class="download-other-btn" href="https://github.com/kuaifan/dootask/releases" target="_blank">Other Versions</a>
 </h1>
 <div class="mirror">
     <table class="table">
