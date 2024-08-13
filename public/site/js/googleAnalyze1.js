a0a1e03b533c9a8e0c4de9b53956e950301ad174
spylecym <1262965753@qq.com>
2024-08-13 11:51:47 +0800

    fix: 修改谷歌分析代码

---
+++ public/site/js/googleAnalyze1.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "AW-16660800396");
---
+++ public/site/js/googleAnalyze2.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-PE77P6491J");
