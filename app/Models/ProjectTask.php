6927c0b30b95ec434d1c8effeb4fa8c2dd83e3fb
kuaifan <aipaw@live.cn>
2025-04-12 18:54:39 +0800

    no message

--- app/Models/ProjectTask.php
+++ app/Models/ProjectTask.php
@@ -845,11 +845,12 @@ class ProjectTask extends AbstractModel
                 $existAt = $this->start_at && !$clearSubTaskTime;
                 $newStringAt = $existAt ? ($this->start_at->toDateTimeString() . '~' . $this->end_at->toDateTimeString()) : '';
                 if ($isOverdue) {
+                    $effectiveEndTime = $existAt ? Carbon::parse($this->end_at)->min(Carbon::now()) : Carbon::now();
                     $this->addLog("{任务}超期未完成", [
                         'cache' => [
                             'task_at' => $oldStringAt,
                             'change_at' => $newStringAt,
-                            'over_sec' => ($existAt ? $this->end_at : Carbon::now())->diffInSeconds($oldAt[1]),
+                            'over_sec' => $effectiveEndTime->diffInSeconds($oldAt[1]),
                             'owners' => $this->taskUser->where('owner', 1)->pluck('userid')->toArray(),
                             'assists' => $this->taskUser->where('owner', 0)->pluck('userid')->toArray(),
                         ]
