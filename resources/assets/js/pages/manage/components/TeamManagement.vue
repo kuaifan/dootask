32f30826b9a87430b1d476339ef7f71a0bcfe2e7
kuaifan <aipaw@live.cn>
2025-06-18 16:29:03 +0800

    perf: 优化已知问题

--- resources/assets/js/pages/manage/components/TeamManagement.vue
+++ resources/assets/js/pages/manage/components/TeamManagement.vue
@@ -3,8 +3,12 @@
         <div class="management-title">
             {{$L('团队管理')}}
             <div class="title-zoom" @click="minBox=!minBox">
-                <i v-if="minBox" class="taskfont">&#xe7d4;</i>
-                <i v-else class="taskfont">&#xe7d3;</i>
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <rect width="18" height="18" x="3" y="3" rx="2"></rect>
+                        <path d="M9 3v18"></path>
+                    </svg>
+                </span>
             </div>
             <div class="title-icon">
                 <Loading v-if="loadIng > 0"/>
