d5a75f887d3ff5e94529b50728b5ac64956b920a
kuaifan <aipaw@live.cn>
2025-08-07 14:22:00 +0800

    perf: 优化抽屉窗口

--- resources/assets/js/components/DrawerOverlay/index.vue
+++ resources/assets/js/components/DrawerOverlay/index.vue
@@ -3,24 +3,28 @@
         ref="modal"
         v-model="show"
         :closable="escClosable"
-        :mask="!isFullscreen"
+        :mask="finalMask"
         :mask-closable="maskClosable"
         :footer-hide="true"
-        :transition-names="transitionNames"
-        :beforeClose="beforeClose"
-        :class-name="className"
-        fullscreen>
-        <div v-if="isFullscreen" class="overlay-body">
-            <slot/>
-        </div>
+        :fullscreen="true"
+        :class-name="finalClassName"
+        :transition-names="finalTransitionNames"
+        :before-close="beforeClose">
         <DrawerOverlayView
-            v-else
-            :placement="placementName"
-            :size="size"
-            :minSize="minSize"
-            :resize="resize"
+            :placement="finalPlacement"
+            :size="finalSize"
+            :minSize="finalMinSize"
+            :resize="finalResize"
             @on-close="close">
-            <slot/>
+            <template v-if="$slots.title" #title>
+                <slot name="title"></slot>
+            </template>
+            <template v-if="$slots.more" #more>
+                <slot name="more"></slot>
+            </template>
+            <template #default>
+                <slot></slot>
+            </template>
         </DrawerOverlayView>
     </Modal>
 </template>
@@ -36,46 +40,55 @@ export default {
             type: Boolean,
             default: false
         },
+        // 是否显示遮罩，留空自动判断
+        mask: {
+            default: null
+        },
+        // 允许点击遮罩关闭
         maskClosable: {
             type: Boolean,
             default: true
         },
+        // 允许按下 ESC 键关闭
         escClosable: {
             type: Boolean,
             default: true
         },
+        // 是否全屏，留空自动判断屏幕宽度小于 768px 时自动变为全屏模式
+        fullscreen: {
+            default: null
+        },
+        // 抽屉放置位置，可选 'right' 或 'bottom'
+        // 在全屏模式下无效，强制为 'bottom'
         placement: {
             validator(value) {
                 return ['right', 'bottom'].includes(value)
             },
             default: 'bottom'
         },
-        forceFullscreen: {
-            type: Boolean,
-            default: false
-        },
-        transitions: {
-            type: Array,
-            default: () => []
-        },
+        // 抽屉的大小，可以是百分比或像素值，默认 100%
+        // 在全屏模式下无效
         size: {
             type: [Number, String],
             default: "100%"
         },
+        // 抽屉的最小大小，单位为像素，默认 300px
+        // 在全屏模式下无效
         minSize: {
             type: Number,
             default: 300
         },
+        // 允许调整大小，默认为 true
+        // 在全屏模式下无效
         resize: {
             type: Boolean,
             default: true
         },
-        drawerClass: {
+        // 自定义类名
+        className: {
             type: String
         },
-        modalClass: {
-            type: String
-        },
+        // 拦截关闭事件的函数
         beforeClose: Function
     },
     data() {
@@ -92,34 +105,55 @@ export default {
         },
     },
     computed: {
-        isFullscreen() {
-            return this.forceFullscreen || (this.windowWidth < 500 && this.placement != 'bottom')
-        },
-        placementName() {
-            return this.isFullscreen ? 'bottom' : this.placement
-        },
-        transitionNames() {
-            if (this.transitions.length > 0) {
-                return this.transitions
+        finalFullscreen() {
+            if (typeof this.fullscreen === 'boolean') {
+                return this.fullscreen
             }
-            return [`drawer-slide-${this.placementName}`, '']
+            return this.windowWidth < 768
         },
-        className() {
-            const array = []
-            if (this.isFullscreen) {
-                array.push("common-drawer-modal")
-                if (this.modalClass) {
-                    array.push(this.modalClass)
-                }
-            } else {
-                array.push("common-drawer-overlay")
-                if (this.drawerClass) {
-                    array.push(this.drawerClass)
-                }
-                array.push(this.placementName)
+        finalMask() {
+            if (typeof this.mask === 'boolean') {
+                return this.mask
+            }
+            return !this.finalFullscreen
+        },
+        finalClassName() {
+            const array = [
+                "common-drawer",
+                `drawer-${this.finalPlacement}`
+            ];
+            if (this.finalFullscreen) {
+                array.push("drawer-fullscreen")
+            }
+            if (this.className) {
+                array.push(this.className)
             }
             return array.join(" ");
         },
+        finalTransitionNames() {
+            return [`drawer-animation-${this.finalPlacement}`, 'drawer-animation-fade']
+        },
+        finalPlacement() {
+            return this.finalFullscreen ? 'bottom' : this.placement
+        },
+        finalSize() {
+            if (this.finalFullscreen) {
+                return "100%"
+            }
+            return this.size
+        },
+        finalMinSize() {
+            if (this.finalFullscreen) {
+                return 0
+            }
+            return this.minSize
+        },
+        finalResize() {
+            if (this.finalFullscreen) {
+                return false
+            }
+            return this.resize
+        }
     },
     methods: {
         close() {
--- resources/assets/js/components/DrawerOverlay/view.vue
+++ resources/assets/js/components/DrawerOverlay/view.vue
@@ -1,11 +1,9 @@
 <template>
     <div ref="body" class="overlay-body" :style="bodyStyle">
-        <div class="overlay-close">
-            <a href="javascript:void(0)" @click.stop="onClose">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26" fill="none" role="img" class="icon fill-current">
-                    <path d="M8.28596 6.51819C7.7978 6.03003 7.00634 6.03003 6.51819 6.51819C6.03003 7.00634 6.03003 7.7978 6.51819 8.28596L11.2322 13L6.51819 17.714C6.03003 18.2022 6.03003 18.9937 6.51819 19.4818C7.00634 19.97 7.7978 19.97 8.28596 19.4818L13 14.7678L17.714 19.4818C18.2022 19.97 18.9937 19.97 19.4818 19.4818C19.97 18.9937 19.97 18.2022 19.4818 17.714L14.7678 13L19.4818 8.28596C19.97 7.7978 19.97 7.00634 19.4818 6.51819C18.9937 6.03003 18.2022 6.03003 17.714 6.51819L13 11.2322L8.28596 6.51819Z" fill="currentColor"></path>
-                </svg>
-            </a>
+        <div class="overlay-close" @click.stop="onClose">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26">
+                <path d="M8.28596 6.51819C7.7978 6.03003 7.00634 6.03003 6.51819 6.51819C6.03003 7.00634 6.03003 7.7978 6.51819 8.28596L11.2322 13L6.51819 17.714C6.03003 18.2022 6.03003 18.9937 6.51819 19.4818C7.00634 19.97 7.7978 19.97 8.28596 19.4818L13 14.7678L17.714 19.4818C18.2022 19.97 18.9937 19.97 19.4818 19.4818C19.97 18.9937 19.97 18.2022 19.4818 17.714L14.7678 13L19.4818 8.28596C19.97 7.7978 19.97 7.00634 19.4818 6.51819C18.9937 6.03003 18.2022 6.03003 17.714 6.51819L13 11.2322L8.28596 6.51819Z" fill="currentColor"></path>
+            </svg>
         </div>
         <ResizeLine
             v-if="resize"
@@ -19,6 +17,14 @@
             @on-change="onChangeResize"/>
         <div class="overlay-content">
             <div class="overlay-content-status"></div>
+            <div v-if="$slots.title || $slots.more" class="overlay-content-header">
+                <div class="overlay-content-header-title">
+                    <slot name="title"/>
+                </div>
+                <div class="overlay-content-header-more">
+                    <slot name="more"/>
+                </div>
+            </div>
             <div class="overlay-content-body"><slot/></div>
             <div class="overlay-content-navigation"></div>
         </div>
