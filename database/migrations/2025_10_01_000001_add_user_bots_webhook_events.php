7c21782ab5440e42fa4aacbd78a696ff538f0d39
kuaifan <aipaw@live.cn>
2025-10-08 04:34:31 +0000

    no message

--- database/migrations/2025_10_01_000001_add_user_bots_webhook_events.php
+++ database/migrations/2025_10_01_000001_add_user_bots_webhook_events.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\\Database\\Migrations\\Migration;
-use Illuminate\\Database\\Schema\\Blueprint;
-use Illuminate\\Support\\Facades\\DB;
-use Illuminate\\Support\\Facades\\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
