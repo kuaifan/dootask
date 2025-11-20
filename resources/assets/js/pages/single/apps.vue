66252072c787ea17b5bda729e67d15b672788007
kuaifan <aipaw@live.cn>
2025-11-20 06:18:56 +0000

    feat: 添加 iframe 测试功能，支持通过 URL 加载外部内容

--- resources/assets/js/pages/single/apps.vue
+++ resources/assets/js/pages/single/apps.vue
@@ -15,6 +15,24 @@ export default {
             return
         }
 
+        // iframe 测试
+        if (name === 'iframe-test') {
+            const {url} = this.$route.query;
+            if (!url) {
+                $A.modalError("URL不能为空");
+                return
+            }
+            await this.$refs.app.onOpen({
+                id: 'iframe-test',
+                name: 'iframe-test',
+                url: url,
+                url_type: 'iframe',
+                transparent: true,
+                keep_alive: false,
+            })
+            return
+        }
+
         const app = (await $A.IDBArray("cacheMicroApps")).reverse().find(item => item.name === name);
         if (!app) {
             $A.modalError("应用不存在");
